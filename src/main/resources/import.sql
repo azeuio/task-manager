04db6210bf65e5ab87586408bff3c22234e45e5f
azeuio <nicolas.mesrine@epitech.eu>
2026-02-16 00:07:17 +0100

    chore: comment out user and project insert statements in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (id, username, email) VALUES (1, 'alice', 'alice@example.com');
-INSERT INTO users (id, username, email) VALUES (2, 'bob', 'bob@example.com');
+-- INSERT INTO users (id, username, email) VALUES (1, 'alice', 'alice@example.com');
+-- INSERT INTO users (id, username, email) VALUES (2, 'bob', 'bob@example.com');
 
-INSERT INTO projects (id, name, owner_id, status) VALUES (1, 'Project 1', 1, 0);
-INSERT INTO projects (id, name, owner_id, status) VALUES (2, 'Project 2', 1, 0);
-INSERT INTO projects (id, name, owner_id, status) VALUES (3, 'Project 3', 2, 0);
+-- INSERT INTO projects (id, name, owner_id, status) VALUES (1, 'Project 1', 1, 0);
+-- INSERT INTO projects (id, name, owner_id, status) VALUES (2, 'Project 2', 1, 0);
+-- INSERT INTO projects (id, name, owner_id, status) VALUES (3, 'Project 3', 2, 0);
 
 -- INSERT INTO tasks (id, title, description, project_id) VALUES (1, 'Task 1', 'Description for Task 1', 1);
 -- INSERT INTO tasks (id, title, description, project_id) VALUES (2, 'Task 2', 'Description for Task 2', 1); 
